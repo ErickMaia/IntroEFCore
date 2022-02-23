f65461b3b32a77f445f930790df5b583fd1730ae
ErickMaia <erickmaiatmariano@gmail.com>
2022-02-23 06:07:31 -0300

    Criados arquivos de configuração individuais para cada mapeamento em Fluent API.

--- IntroEFCore/Data/ApplicationContext.cs
+++ IntroEFCore/Data/ApplicationContext.cs
@@ -17,57 +17,7 @@ namespace IntroEFCore.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Cliente>(
-                buildAction => {
-                    buildAction.ToTable("Clientes");
-                    buildAction.HasKey(p => p.Id); 
-                    buildAction.Property(p => p.Nome).HasColumnType("VARCHAR(80)") .IsRequired();
-                    buildAction.Property(p => p.Telefone).HasColumnType("CHAR(11)"); 
-                    buildAction.Property(p => p.CEP).HasColumnType("CHAR(8)").IsRequired(); 
-                    buildAction.Property(p => p.Estado).HasColumnType("CHAR(2)").IsRequired();
-                    buildAction.Property(p => p.Cidade).HasMaxLength(60).IsRequired(); 
-
-                    buildAction.HasIndex(p => p.Telefone).HasDatabaseName("idx_cliente_telefone"); 
-
-                }
-            ); 
-
-            modelBuilder.Entity<Produto>(
-                buildAction => {
-                    buildAction.ToTable("Produtos"); 
-                    buildAction.HasKey(p => p.Id); 
-                    buildAction.Property(p => p.CodigoBarras).HasColumnType("VARCHAR(14)").IsRequired();
-                    buildAction.Property(p => p.Descricao).HasColumnType("VARCHAR(60)"); 
-                    buildAction.Property(p => p.Valor).IsRequired(); 
-                    buildAction.Property(p => p.TipoProduto).HasConversion<string>();  
-                }
-            ); 
-
-            modelBuilder.Entity<Pedido>(
-                buildAction => {
-                    buildAction.ToTable("Pedidos"); 
-                    buildAction.HasKey(p => p.Id); 
-                    buildAction.Property(p => p.IniciadoEm).HasDefaultValue("GETDATE()").ValueGeneratedOnAdd(); 
-                    buildAction.Property(p => p.StatusPedido).HasConversion<string>(); 
-                    buildAction.Property(p => p.TipoFrete).HasConversion<int>(); 
-                    buildAction.Property(p => p.Observacao).HasColumnType("VARCHAR(512)"); 
-
-                    buildAction.HasMany(p => p.Itens)
-                        .WithOne(p => p.Pedido)
-                        .OnDelete(DeleteBehavior.Cascade); 
-                }
-
-            );
-
-            modelBuilder.Entity<PedidoItem>(
-                buildAction => {
-                    buildAction.ToTable("PedidoItens"); 
-                    buildAction.HasKey(p => p.Id); 
-                    buildAction.Property(p => p.Quantidade).HasDefaultValue(1).IsRequired(); 
-                    buildAction.Property(p => p.Desconto).IsRequired(); 
-                    buildAction.Property(p => p.Valor).IsRequired(); 
-                }
-            ); 
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly); 
         }
 
     }
---
+++ IntroEFCore/Data/Configurations/ClienteConfiguration.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IntroEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IntroEFCore.Data.Configurations
+{
+    public class ClienteConfiguration : IEntityTypeConfiguration<Cliente>
+    {
+        public void Configure(EntityTypeBuilder<Cliente> builder)
+        {
+            builder.ToTable("Clientes");
+            builder.HasKey(p => p.Id); 
+            builder.Property(p => p.Nome).HasColumnType("VARCHAR(80)") .IsRequired();
+            builder.Property(p => p.Telefone).HasColumnType("CHAR(11)"); 
+            builder.Property(p => p.CEP).HasColumnType("CHAR(8)").IsRequired(); 
+            builder.Property(p => p.Estado).HasColumnType("CHAR(2)").IsRequired();
+            builder.Property(p => p.Cidade).HasMaxLength(60).IsRequired(); 
+
+            builder.HasIndex(p => p.Telefone).HasDatabaseName("idx_cliente_telefone"); 
+        }
+    }
+}
