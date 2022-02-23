54028e855979ac755671869fab4803648f298b66
ErickMaia <erickmaiatmariano@gmail.com>
2022-02-23 04:33:15 -0300

    Criada classe de contexto

--- IntroEFCore/Data/ApplicationContext.cs
+++ IntroEFCore/Data/ApplicationContext.cs
@@ -8,6 +8,10 @@ namespace IntroEFCore.Data
 {
     public class ApplicationContext : DbContext
     {
-        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Server=LAPTOP-G4OVP1BV\\SQLEXPRESS;Database=IntroEFCore;User Id=admin;Password=123;"; 
+            optionsBuilder.UseSqlServer(connectionString); 
+        }
     }
 }
