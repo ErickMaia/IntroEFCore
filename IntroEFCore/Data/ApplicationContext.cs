568a2c3ce37ea61972a6caf1fd6c673a4697ce2f
ErickMaia <erickmaiatmariano@gmail.com>
2022-03-02 07:49:12 -0300

    Inserindo registros em massa

--- IntroEFCore/Data/ApplicationContext.cs
+++ IntroEFCore/Data/ApplicationContext.cs
@@ -4,16 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using IntroEFCore.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace IntroEFCore.Data
 {
     public class ApplicationContext : DbContext
     {
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole()); 
         public DbSet<Produto> Produtos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string connectionString = "Server=LAPTOP-G4OVP1BV\\SQLEXPRESS;Database=IntroEFCore;User Id=admin;Password=123;"; 
-            optionsBuilder.UseSqlServer(connectionString); 
+            optionsBuilder
+                .UseLoggerFactory(_logger)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer(connectionString); 
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- IntroEFCore/Program.cs
+++ IntroEFCore/Program.cs
@@ -6,38 +6,58 @@ using Microsoft.EntityFrameworkCore;
 
 var context = new ApplicationContext(); 
 
-//MIGRAÇÕES
+// //MIGRAÇÕES
 
-Console.WriteLine("\n-- Migrações que não foram rodadas no banco de dados -- \n");
+// Console.WriteLine("\n-- Migrações que não foram rodadas no banco de dados -- \n");
 
-var pendingMigrations = context.Database.GetPendingMigrations(); 
+// var pendingMigrations = context.Database.GetPendingMigrations(); 
 
-int contadorDeMigrations = 0; 
+// int contadorDeMigrations = 0; 
 
-foreach (var migration in pendingMigrations)
-{
-    contadorDeMigrations++; 
-    Console.WriteLine($"Migration Nº{contadorDeMigrations}: {migration}");
-}
+// foreach (var migration in pendingMigrations)
+// {
+//     contadorDeMigrations++; 
+//     Console.WriteLine($"Migration Nº{contadorDeMigrations}: {migration}");
+// }
 
-Console.WriteLine("\n-- Fim da lista de migrações que não foram rodadas no banco de dados -- \n\n");
+// Console.WriteLine("\n-- Fim da lista de migrações que não foram rodadas no banco de dados -- \n\n");
 
-//ADICIONANDO PRODUTO
+// //ADICIONANDO PRODUTO
 
-var produto = new Produto(
-    "12852934049",
-    "Produto de teste EFCore", 
-    20, 
-    TipoProduto.MercadoriaParaRevenda, 
-    true); 
+// var produto = new Produto(
+//     "12852934049",
+//     "Produto de teste EFCore", 
+//     20, 
+//     TipoProduto.MercadoriaParaRevenda, 
+//     true); 
 
-//diferentes formas de adicionar
-context.Produtos.Add(produto); 
-context.Add(produto); 
-context.Set<Produto>().Add(produto); 
-context.Entry(produto).State = EntityState.Added; //não depende de DbSet
+// //diferentes formas de adicionar
+// context.Produtos.Add(produto); 
+// context.Add(produto); 
+// context.Set<Produto>().Add(produto); 
+// context.Entry(produto).State = EntityState.Added; //não depende de DbSet
 
-//salva 1 registro apenas apesar dos 4 comandos acima, pois o rastreamento é feito por instância
-int registrosAfetados = context.SaveChanges(); 
+// //salva 1 registro apenas apesar dos 4 comandos acima, pois o rastreamento é feito por instância
+// int registrosAfetados = context.SaveChanges(); 
 
-System.Console.WriteLine($"Foram inseridos {registrosAfetados} registros. ");
+// System.Console.WriteLine($"Foram inseridos {registrosAfetados} registros. ");
+
+//INSERINDO REGISTROS EM MASSA
+
+var listAProdutos = new List<Produto>(){
+    new Produto(
+        "4923837571",
+        "Produto de teste EFCore 2", 
+        50, 
+        TipoProduto.MercadoriaParaRevenda, 
+        true), 
+    new Produto(
+        "9203848123",
+        "Produto de teste EFCore 3", 
+        46, 
+        TipoProduto.Servico, 
+        false)
+}; 
+
+context.Produtos.AddRange(listAProdutos); 
+context.SaveChanges(); 
